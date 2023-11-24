3d17867eaf75fca5750072025359f970881f47d5
mrmoi31 <dodo.leva.dl@gmail.com>
2023-11-24 08:05:26 +0100

    ajout d'un form autour du tableau

--- medecins.php
+++ medecins.php
@@ -22,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
         <h2>Médecins</h2>
         <?php require "header.html";?>
         <div class="tablo">
+        <form action="index.php" method="post" class="tab-med">
         <table id="example">
             <thead>
                 <tr>
@@ -42,46 +43,48 @@ document.addEventListener('DOMContentLoaded', function() {
             </thead>
             <tbody>
             <tr>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
+                <td>test1</td>
+                <td>test1</td>
+                <td>test1</td>
+                <td>test1</td>
+                <td>test1</td>
+                <td>test1</td>
+                <td>test1</td>
+                <td>test1</td>
+                <td>test1</td>
+                <td>test1</td>
             </tr>
             <tr>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
+                <td>test2</td>
+                <td>test2</td>
+                <td>test2</td>
+                <td>test2</td>
+                <td>test2</td>
+                <td>test2</td>
+                <td>test2</td>
+                <td>test2</td>
+                <td>test2</td>
+                <td>test2</td>
+                <?php //echo '<td><input type="checkbox" name="selectedRows[]" value="' . $row['Id_Patient'] . '"></td>'; ?>
             </tr>
             <tr>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
-                <td>test</td>
+                <td>test3</td>
+                <td>test3</td>
+                <td>test3</td>
+                <td>test3</td>
+                <td>test3</td>
+                <td>test3</td>
+                <td>test3</td>
+                <td>test3</td>
+                <td>test3</td>
+                <td>test3</td>
             </tr>
             </tbody>
         </table>
+    </form>
     </div>
     <div class="ams">
-        <form action="index.php" method="get">
+        <form action="index.php" method="post">
         <button type="submit" name="choix" value="Ajouter">Ajouter</button>
         <button type="submit" name="choix" value="Modifier" class="amsbouton">Modifier</button>
         <button type="submit" name="choix" value="Supprimer" class="amsbouton">Supprimer</button>
