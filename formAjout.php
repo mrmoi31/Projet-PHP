cd3c2f5dc81bbc73c15b04124fcd42e51ecc453c
Theobasket29 <blancotheo29@gmail.com>
2023-12-01 09:11:50 +0100

    Debut creation formulaire

---
+++ formAjout.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+        <title>Medecins - Cabinet Médical</title>
+    </head>
+    <body>
+
+        <form action="login.php" method="post">
+        <label class="log">Login :</label>
+        <input type="text" name="login" id="login"><br>
+
+        <label class="log">Mot de passe :</label>
+        <input type="text" name="mdp" id="mdp"><br>
+
+        <button type="submit" name="send" value="send">Envoyer</button>
+        <button type="reset" name="reset" value="reset">Reset</button>
+        </form>
+
+
+
+
+
+
+
+    </body>
