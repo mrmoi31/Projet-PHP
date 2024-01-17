903d66904dc4226301226f2bbea3e796d716ae75
Theobasket29 <blancotheo29@gmail.com>
2024-01-17 15:06:42 +0100

    Update FormAjout

--- formAjout.php
+++ formAjout.php
@@ -7,14 +7,10 @@
     </head>
     <?php 
         require "header.html";
-        require "test.php";
-        $civilite = $_POST["civilite"];
+        include "connexionBd.php";
     ?>
     <body>
-       
-        
-
-        <form action="testTheo.php" method="post">
+        <form method="post">
         <label>Civilite :</label>
         <input type="text" name="civ" id="civ"><br>
 
@@ -24,15 +20,40 @@
         <label>Prenom :</label>
         <input type="text" name="prenom" id="prenom"><br>
 
-        <button type="submit" name="send" value="send" action="ajoutTest()">Envoyer</button>
+        <button type="submit" name="send" value="send">Envoyer</button>
         <button type="reset" name="reset" value="reset">Reset</button>
         </form>
 
+       
 
 
+</body>
+    <?php
+     //Recupérer les données
+     $civilite = $_POST["civilite"];
+     $nom = $_POST["nom"];
+     $prenom = $_POST["prenom"];
 
+     //Vérication de doublon
+     $sql = "SELECT * FROM medecin WHERE civilite = '$civilite' AND nom = '$nom' AND prenom = '$prenom'";
+     $result = $linkpdo->query($sql);
+ 
+     if ($result->rowCount() > 0 ){
+         echo "Ce medecin existe deja dans la BD.";
+     }else{
+         $insertSql = "INSERT INTO medecin(civilite, nom, prenom) VALUES('$civilite', '$nom', '$prenom')";
+ 
+         if ($linkpdo->exec($insertSql) !== false){
+             echo "Medecin enregistrer";
+         }else{
+             echo "Erreur lors de l'insertion du medecin : " . print_r($linkpdo->errorInfo());
+         }
+     } 
 
+ 
+      
+       
 
-
-    </body>
+       
+        ?>
 </html>
