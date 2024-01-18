c4f68c245b191bb8628c234adea4d300d50e3ea7
Theobasket29 <blancotheo29@gmail.com>
2024-01-18 09:04:41 +0100

    ajout medecin fonctionnel

--- formAjout.php
+++ formAjout.php
@@ -8,11 +8,15 @@
     <?php 
         require "header.html";
         include "connexionBd.php";
+ 
     ?>
     <body>
         <form method="post">
-        <label>Civilite :</label>
-        <input type="text" name="civ" id="civ"><br>
+        <label>Civilite :</label><br>
+        <label>M :</label>
+        <input type="radio" name="civilite" id="civilite" value="M">
+        <label>Mme :</label>
+        <input type="radio" name="civilite" id="civilite" value="Mme"><br>
 
         <label>Nom :</label>
         <input type="text" name="nom" id="nom"><br>
@@ -24,36 +28,35 @@
         <button type="reset" name="reset" value="reset">Reset</button>
         </form>
 
-       
+  
 
 
 </body>
-    <?php
-     //Recupérer les données
-     $civilite = $_POST["civilite"];
-     $nom = $_POST["nom"];
-     $prenom = $_POST["prenom"];
 
-     //Vérication de doublon
-     $sql = "SELECT * FROM medecin WHERE civilite = '$civilite' AND nom = '$nom' AND prenom = '$prenom'";
-     $result = $linkpdo->query($sql);
- 
-     if ($result->rowCount() > 0 ){
-         echo "Ce medecin existe deja dans la BD.";
+<?php  
+ //Recupérer les données
+ $civilite = $_POST["civilite"];
+ $nom = $_POST["nom"];
+ $prenom = $_POST["prenom"];
+
+ //Vérication de doublon
+ $sql = "SELECT * FROM medecin WHERE civilite = '$civilite' AND nom = '$nom' AND prenom = '$prenom'";
+ $result = $linkpdo->query($sql);
+
+ if ($result->rowCount() > 0 ){
+     echo "Ce medecin existe deja dans la BD.";
+ }else{
+     $insertSql = "INSERT INTO medecin(civilite, nom, prenom) VALUES('$civilite', '$nom', '$prenom')";
+
+     if ($linkpdo->exec($insertSql) !== false){
+         echo "Medecin enregistrer";
      }else{
-         $insertSql = "INSERT INTO medecin(civilite, nom, prenom) VALUES('$civilite', '$nom', '$prenom')";
- 
-         if ($linkpdo->exec($insertSql) !== false){
-             echo "Medecin enregistrer";
-         }else{
-             echo "Erreur lors de l'insertion du medecin : " . print_r($linkpdo->errorInfo());
-         }
-     } 
+         echo "Erreur lors de l'insertion du medecin : " . print_r($linkpdo->errorInfo());
+     }
+ } 
 
- 
-      
-       
 
-       
-        ?>
+   ?>
+
+   
 </html>
