d292fa5bbb366281a8c1e1bc31df02e0b1457838
Theobasket29 <blancotheo29@gmail.com>
2024-01-19 09:15:11 +0100

    ajout de medecin FINI

--- formAjout.php
+++ formAjout.php
@@ -33,31 +33,10 @@
 
 </body>
 
-<?php  
- include "connexionBd.php";
- //Recupérer les données
- $civilite = $_POST["civilite"];
- $nom = $_POST["nom"];
- $prenom = $_POST["prenom"];
-
- //Vérication de doublon
- $sql = "SELECT * FROM medecin WHERE civilite = '$civilite' AND nom = '$nom' AND prenom = '$prenom'";
- $result = $linkpdo->query($sql);
-
- if ($result->rowCount() > 0 ){
-     echo "Ce medecin existe deja dans la BD.";
- }else{
-     $insertSql = "INSERT INTO medecin(civilite, nom, prenom) VALUES('$civilite', '$nom', '$prenom')";
-
-     if ($linkpdo->exec($insertSql) !== false){
-         echo "Medecin enregistrer";
-     }else{
-         echo "Erreur lors de l'insertion du medecin : " . print_r($linkpdo->errorInfo());
-     }
- } 
-
-
-   ?>
+<?php include "testTheo.php";
+if (isset($_POST['send'])) {
+    ajoutMedecin();
+} ?>
 
    
 </html>
--- testTheo.php
+++ testTheo.php
@@ -1,45 +1,28 @@
-<?php
+<?php  
 
-include "connexionBd.php";
+function ajoutMedecin(){
+ include "connexionBd.php";
+ //Recupérer les données
+ $civilite = isset($_POST["civilite"]) ? $_POST["civilite"] : '';
+    $nom = isset($_POST["nom"]) ? $_POST["nom"] : '';
+    $prenom = isset($_POST["prenom"]) ? $_POST["prenom"] : '';
 
-function ajoutTest(){
-    
-  
-    //Recupérer les données
-    $civilite = $_POST["civilite"];
-    $nom = $_POST["nom"];
-    $prenom = $_POST["prenom"];
+ //Vérication de doublon
+ $sql = "SELECT * FROM medecin WHERE civilite = '$civilite' AND nom = '$nom' AND prenom = '$prenom'";
+ $result = $linkpdo->query($sql);
 
-    //Vérication de doublon
-    $sql = "SELECT * FROM medecin WHERE civilite = '$civilite' AND nom = '$nom' AND prenom = '$prenom'";
-    $result = $linkpdo->query($sql);
+ if ($result->rowCount() > 0 ){
+     echo "Ce medecin existe deja dans la BD.";
+ }else{
+     $insertSql = "INSERT INTO medecin(civilite, nom, prenom) VALUES('$civilite', '$nom', '$prenom')";
 
-   
-
-    if ($result->rowCount() > 0 ){
-        echo "Ce medecin existe deja dans la BD.";
-    }else{
-        $insertSql = "INSERT INTO medecin(civilite, nom, prenom) VALUES('$index+1','$civilite', '$nom', '$prenom')";
-        $index = "SELECT count(id_medecin)";
-        if ($linkpdo->exec($insertSql) !== false){
-            echo "Medecin enregistrer";
-        }else{
-            echo "Erreur lors de l'insertion du medecin : " . print_r($linkpdo->errorInfo());
-        }
-    } 
+     if ($linkpdo->exec($insertSql) !== false){
+         echo "Medecin enregistrer";
+     }else{
+         echo "Erreur lors de l'insertion du medecin : " . print_r($linkpdo->errorInfo());
+     }
+ } }
 
 
-     
-      
-
-      
-       
-
-  
-   
-
-   
-    
-}
-?>
+   ?>
 
