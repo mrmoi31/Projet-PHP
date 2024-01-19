8ceb4a680c1824f816642f5f37b006843e734051
Theobasket29 <blancotheo29@gmail.com>
2024-01-19 18:50:14 +0100

    creation cookie

--- login.php
+++ login.php
@@ -1,4 +1,15 @@
 <?php 
+
+// Définir les informations à stocker dans le cookie
+$login = "utilisateur123";
+$motDePasse = "motdepasse123";
+
+// Créer le cookie avec une durée de validité d'une heure (3600 secondes)
+setcookie("login", $login, time() + 3600, "/");
+setcookie("motDePasse", $motDePasse, time() + 3600, "/");
+
+echo "Cookies créés avec succès!";
+
 //code cookie a mettre au dessus de ce comm
 header("Location: ./menu.php");
 exit;
