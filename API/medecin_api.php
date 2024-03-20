1e60c218803c72328c7456c4a111bd6fe2f96da5
mrmoi31 <dodo.leva.dl@gmail.com>
2024-03-20 08:33:04 +0100

    maj api medecin

--- API/medecin_api.php
+++ API/medecin_api.php
@@ -24,12 +24,31 @@ include 'APIMedecin.php';
 		    $prenom = $data['prenom'];
 
 		    $res = ajoutMedecin($civilite, $nom, $prenom);
-		    deliver_response("200", "OK", $res);
+		    if (!$res) {
+		    	deliver_response("200", "OK", $res);
+		    } else {
+		    	deliver_response("400", "probleme de requete");
+		    }
+
  			break;
  		
 		case "DELETE" :
 
-				supprimerMedecin($id);
+				$data = (array) json_decode(file_get_contents('php://input'), TRUE);
+
+				if (!isset($data['id'])) {
+					deliver_response(400, "Données manquantes");
+				} else {
+
+					$id = $data['id'];
+
+					$res = supprimerMedecin($id);
+					if (!$res) {
+		   		 	deliver_response("200", "OK", $res);
+		   			} else {
+		   		 	deliver_response("400", "probleme de requete");
+		   		 	}
+				}
 		
 			break;
 
