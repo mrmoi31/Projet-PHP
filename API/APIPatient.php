508f8ab1febc5a6bb0a4883004653007097af64d
mrmoi31 <dodo.leva.dl@gmail.com>
2024-03-29 11:37:56 +0100

    maj api patient

--- API/APIPatient.php
+++ API/APIPatient.php
@@ -44,20 +44,24 @@ function getAllPatient(){
     return $res;
 
 }
+
+function getPatientById($id)
+{
+    $linkpdo = connexionBdGen();
+
+    $stmt = $linkpdo->prepare("SELECT * FROM `usager` where id_usager = :id;");
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    $res = ($stmt->fetchAll(PDO::FETCH_ASSOC));
+    $linkpdo = null;
+    return $res;
+}
+
 function ajoutPatient(){
-        include "connexionBd.php";
+        //include "connexionBd.php";
         //Recupérer les données
-        $civilite = isset($_POST["civilite"]) ? $_POST["civilite"] : '';
-        $nom = isset($_POST["nom"]) ? $_POST["nom"] : '';
-        $prenom = isset($_POST["prenom"]) ? $_POST["prenom"] : '';
-        $adresse = isset($_POST["adresse"]) ? $_POST["adresse"] : '';
-        $ville = isset($_POST["ville"]) ? $_POST["ville"] : '';
-        $code_postal = isset($_POST["codePostal"]) ? $_POST["codePostal"] : '';
-        $dateN = isset($_POST["dateNaissance"]) ? $_POST["dateNaissance"] : '';
-        $lieuxN = isset($_POST["lieuNaissance"]) ? $_POST["lieuNaissance"] : '';
-        $numSecu = isset($_POST["numSecu"]) ? $_POST["numSecu"] : '';
-        $id_medecin = isset($_POST["id_medecin"]) ? $_POST["id_medecin"] : '';
         
+        $linkpdo = connexionBdGen();
 
          //Vérication de doublon
          $sql = "SELECT * FROM patient WHERE civilite = '$civilite' AND nom = '$nom' AND prenom = '$prenom' AND adresse = '$adresse' AND codePostal = '$code_postal' AND ville = '$ville' AND dateNaissance = '$dateN' AND lieuNaissance = '$lieuxN' AND numSecu = '$numSecu' ";
@@ -73,7 +77,42 @@ function ajoutPatient(){
              }else{
                  echo "Erreur lors de l'insertion du patient : " . print_r($linkpdo->errorInfo());
              }
-         }}
+         }
+     }
+
+     function ajoutMedecin($civilite, $nom, $prenom, $adresse, $ville, $codePostal, $dateN, $lieuN, $numSecu, $id_medecin){
+        //include "connexionBd.php";
+     
+        $linkpdo = connexionBdGen();
+        
+        $stmt = $linkpdo->prepare("SELECT * FROM `medecin` WHERE civilite = :civilite AND nom = :nom AND prenom =   :prenom");
+        $stmt->bindParam(':civilite', $civilite);
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':prenom', $prenom);
+        $res = $stmt->execute();
+    
+    if ($res == true ) {
+        if ($stmt->rowCount() > 0 ){
+           echo "Ce medecin existe deja dans la BD.\n";
+        }else{
+            
+               $stmt = "INSERT INTO medecin(civilite, nom, prenom) VALUES(:civilite, :nom, :prenom)";
+               $stmt = $linkpdo->prepare($stmt);
+               $stmt->bindParam(':civilite', $civilite);
+               $stmt->bindParam(':nom', $nom);
+               $stmt->bindParam(':prenom', $prenom);
+
+            if ($stmt->execute() != false){
+                echo "Medecin enregistré\n";
+            }else{
+                echo "Erreur lors de l'insertion du medecin : " . print_r($linkpdo->errorInfo());
+            }
+        }
+    }else{
+        deliver_response(400, "Erreur SQL");    
+    }
+}
+
 function supprimerPatient($id) {
     //include "connexionBd.php";
 
--- API/patient_api.php
+++ API/patient_api.php
@@ -16,28 +16,34 @@ include 'APIPatient.php';
  			
  			break;
 
-			/*
-			case 'POST':
+			
+		case 'POST':
  			$data = (array) json_decode(file_get_contents('php://input'), TRUE);
 
 	        if (!isset($data['civilite']) || !isset($data['nom'] || !isset($data['prenom']))) {
 	            deliver_response(400, "Données manquantes");
 		    }else{
 
-		    $civilite = $data['civilite'];
-		    $nom = $data['nom'];
-		    $prenom = $data['prenom'];
+	        $civilite = isset($_POST["civilite"]) ? $_POST["civilite"] : '';
+	        $nom = isset($_POST["nom"]) ? $_POST["nom"] : '';
+	        $prenom = isset($_POST["prenom"]) ? $_POST["prenom"] : '';
+	        $adresse = isset($_POST["adresse"]) ? $_POST["adresse"] : '';
+	        $ville = isset($_POST["ville"]) ? $_POST["ville"] : '';
+	        $code_postal = isset($_POST["codePostal"]) ? $_POST["codePostal"] : '';
+	        $dateN = isset($_POST["dateNaissance"]) ? $_POST["dateNaissance"] : '';
+	        $lieuxN = isset($_POST["lieuNaissance"]) ? $_POST["lieuNaissance"] : '';
+	        $numSecu = isset($_POST["numSecu"]) ? $_POST["numSecu"] : '';
+	        $id_medecin = isset($_POST["id_medecin"]) ? $_POST["id_medecin"] : '';
 
-		    $res = ajoutMedecin($civilite, $nom, $prenom);
+
+		    $res = ajoutPatient($civilite, $nom, $prenom, $adresse, $ville, $codePostal, $dateN, $lieuxN, $numSecu, $id_medecin);
 		    if (!$res) {
 		    	deliver_response("200", "OK", $res);
 		    } else {
 		    	deliver_response("400", "probleme de requete");
 		    }
 
- 			break;
- 			*/
-			
+ 		break;		
 
  		
 		case 'DELETE' :
